b9bb65933b57f62779acf28817f2b86a9ea50aac
m0zu <m0zurillex@gmail.com>
2019-06-12 20:39:39 +0900

    不要なファイルの削除

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from dataTraffic import dataTraffic
-
-telnum = os.environ.get('telnum')
-password = os.environ.get('password')
-line_access_token = os.environ.get('line_access_token')
-
-dt = dataTraffic(telnum=telnum, password=password, line_access_token=line_access_token)
-dt.send_data()
