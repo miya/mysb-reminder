4d96881ac46e9687dc08b6681a3fba843be4eb8a
m0zu <m0zurillex@gmail.com>
2019-10-03 16:26:07 +0900

    軽微な修正

--- MySBdT/dataTraffic.py
+++ MySBdT/dataTraffic.py
@@ -11,29 +11,29 @@ class dataTraffic:
         self.line_access_token = line_access_token
 
     def login(self):
-        session = requests.Session()
-        req = session.get('https://my.softbank.jp/msb/d/webLink/doSend/MSB020063')
-        soup = BeautifulSoup(req.text, 'lxml')
+        s = requests.Session()
+        r = s.get('https://my.softbank.jp/msb/d/webLink/doSend/MSB020063')
+        soup = BeautifulSoup(r.text, 'lxml')
         ticket = soup.find('input', type='hidden').get('value')
         payload = {
             'telnum': self.telnum,
             'password': self.password,
             'ticket': ticket
         }
-        session.post('https://id.my.softbank.jp/sbid_auth/type1/2.0/login.php', data=payload)
-        return session
+        s.post('https://id.my.softbank.jp/sbid_auth/type1/2.0/login.php', data=payload)
+        return s
 
     def get(self):
-        session = self.login()
-        req = session.get('https://my.softbank.jp/msb/d/webLink/doSend/MRERE0000')
-        soup = BeautifulSoup(req.text, 'lxml')
+        s = self.login()
+        r= s.get('https://my.softbank.jp/msb/d/webLink/doSend/MRERE0000')
+        soup = BeautifulSoup(r.text, 'lxml')
         auth_token = soup.find_all('input')
         payload = {
             'mfiv': auth_token[0].get('value'),
             'mfsb': auth_token[1].get('value'),
         }
-        req2 = session.post('https://re11.my.softbank.jp/resfe/top/', data=payload)
-        soup2 = BeautifulSoup(req2.text, 'lxml')
+        r2 = s.post('https://re11.my.softbank.jp/resfe/top/', data=payload)
+        soup2 = BeautifulSoup(r2.text, 'lxml')
         num = [float(re.findall('\d+[.]+\d\d', str(i))[0]) for i in soup2.find_all(class_='p-left-10')]
         total = num[1]
         remain = num[1] - num[0]
@@ -44,9 +44,9 @@ class dataTraffic:
     def line(self):
         data = self.get()
         text = '{}GB / {}GB ({}%)'.format(data[1], data[0], data[3])
-        print(text)
         line_notify_token = self.line_access_token
         line_notify_api = 'https://notify-api.line.me/api/notify'
         payload = {'message': text}
         headers = {'Authorization': 'Bearer ' + line_notify_token}
         line_notify = requests.post(line_notify_api, data=payload, headers=headers)
+        return line_notify.status_code
