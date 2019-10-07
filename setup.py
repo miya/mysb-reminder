0c9ea6a4f2303326e3052b7a49fbb7d9184cfc0a
m0zu <m0zurillex@gmail.com>
2019-10-07 23:31:18 +0900

    Version 2.1.1 Release🎍

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_reqs = [line.strip() for line in open('requirements.txt').readlines()]
 
 setup(
     name='MySB_datatraffic',
-    version='2.1.0',
+    version='2.1.1',
     description='MySoftBank dataTraffic',
     url='https://github.com/0x0u/MySB_dataTraffic',
     author='m0zu',
