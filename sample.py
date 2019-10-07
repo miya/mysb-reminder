bf4e41b5273e39815246d0ac3a5a0c3df48b78d7
m0zu <m0zurillex@gmail.com>
2019-10-08 00:02:17 +0900

    サンプルコードの修正

--- sample.py
+++ sample.py
@@ -1,13 +1,14 @@
-from MySBdT import dataTraffic
+import MySBdt
 
 telnum = 'your_phone_number'
 password = 'your_mysoftbank_password'
 line_access_token = 'your_line_notify_token'
 
-dt = dataTraffic(telnum=telnum, password=password, line_access_token=line_access_token)
+# インスタンスの作成
+api = MySBdt.API(telnum=telnum, password=password, line_access_token=line_access_token)
 
 # データ（総量、残量、使用量、割合、前月繰越分）の取得
-dt.get()
+api.get()
 
-# LINEに通知する
-dt.line()
+# lineに通知する
+api.line()
