47e839076738fd0b68772400dd5bc06eecd8fd62
0x0u <Yf8w6fyjL4tNMaEY6uRW6kG56_m7sf>
2020-01-27 03:03:44 +0900

    データ量取得部のソースコードを大幅修正

--- MySBdt/MySBdt.py
+++ MySBdt/MySBdt.py
@@ -1,4 +1,5 @@
 import re
+import ast
 import requests
 from bs4 import BeautifulSoup
 
@@ -32,22 +33,49 @@ class API:
             'mfsb': auth_token[1].get('value'),
         }
         r2 = s.post('https://re11.my.softbank.jp/resfe/top/', data=payload)
-        soup2 = BeautifulSoup(r2.text, 'lxml')
-        num = [float(re.findall('\d+[.]+\d\d', str(i))[0]) for i in soup2.find_all(class_='p-left-10')]
-        if num == []:
-            total, remain, used, rate, bf = 0.00, 0.00, 0.00, 0.00, 0.00
-        else:
-            total = round(num[1], 2)
-            remain = round(num[1] - num[0], 2)
-            used = round(num[0], 2)
-            rate = round(remain / total * 100, 1)
-            bf = round(num[2], 2)
-        dt = {'total':str(total).ljust(4, str(0)), 'remain':str(remain).ljust(4, str(0)), 'used':str(used).ljust(4, str(0)), 'rate':str(rate).ljust(4, str(0)), 'bf':str(bf).ljust(4, str(0))}  
-        return dt
+        find_data = re.findall('chartType":  "pie",(.+),}]};', r2.text)[0]
+        num_data_tmp = "{" + find_data + "}"
+        data = ast.literal_eval(num_data_tmp)
+
+        # 使用量
+        used_data = "0" if data["usedGigaData"] == "" else data["usedGigaData"]
+
+        # 残量
+        remain_data = "0" if data["remainData"] == "" else data["remainData"]
+
+        # 繰越データ残量
+        step_remain_data = "0" if data["stepRemainData"] == "" else data["stepRemainData"]
+
+        # 追加データ量
+        additional_data = "0" if data["currentAdditionalData"] == "" else data["currentAdditionalData"]
+
+        # 追加データ使用量
+        additional_used_data = "0" if data["currentAdditionalUsedData"] == "" else data["currentAdditionalUsedData"]
+
+        # 追加繰越データ量
+        given_data = "0" if data["currentGivenData"] == "" else data["currentGivenData"]
+
+        # 追加繰越使用量
+        given_used_data = "0" if data["currentGivenUsedData"] == "" else data["currentGivenUsedData"]
+
+        dic = {
+            "used_data": float(used_data),
+            "remain_data": float(remain_data),
+            "step_remain_data": float(step_remain_data),
+            "additional_data": float(additional_data),
+            "additional_used_data": float(additional_used_data),
+            "given_data": float(given_data),
+            "given_used_data": float(given_used_data),
+        }
+
+        return dic
 
     def line(self):
-        dt = self.get()
-        text = '\n{}GB / {}GB ({}%)'.format(dt['remain'], dt['total'], dt['rate'])
+        data = self.get()
+        remain = data["remain_data"]
+        total = 5.0 + data["step_remain_data"] + data["additional_data"] + data["given_data"]
+        rate = round(remain / total * 100, 2)
+        text = '\n{}GB / {}GB ({}%)'.format(remain, total, rate)
         line_notify_token = self.line_access_token
         line_notify_api = 'https://notify-api.line.me/api/notify'
         payload = {'message': text}
