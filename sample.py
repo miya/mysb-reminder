55b1519909ae2cd669d6af32c662969be62c3f6d
m0zu <m0zurillex@gmail.com>
2020-01-28 15:13:24 +0900

    サンプルコードの編集

--- sample.py
+++ sample.py
@@ -5,10 +5,10 @@ password = 'your_mysoftbank_password'
 line_access_token = 'your_line_notify_token'
 
 # インスタンスの作成
-api = MySBdt.API(telnum=telnum, password=password, line_access_token=line_access_token)
+api = MySBdt.API(telnum=telnum, password=password, line_access_token=line_access_token, current_month_data=5.0)
 
 # データ（総量、残量、使用量、割合、前月繰越分）の取得
-api.get()
+api.get_data()
 
 # lineに通知する
-api.line()
+api.send_message()
