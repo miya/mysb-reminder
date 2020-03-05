6d41521ab48579e2db05ca137e9df8e3d135d220
m0zu <m0zurillex@gmail.com>
2020-03-05 19:50:25 +0900

    Version 4.1 release🎍

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_reqs = [line.strip() for line in open('requirements.txt').readlines()]
 
 setup(
     name='MySB_datatraffic',
-    version='4.0',
+    version='4.1',
     description='MySoftBank dataTraffic',
     url='https://github.com/0x0u/MySB_dataTraffic',
     author='m0zu',
