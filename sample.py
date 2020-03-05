df69d269a09deae9612b4c07d4834fce2b19f91b
m0zu <m0zurillex@gmail.com>
2020-03-05 19:43:02 +0900

    不要な引数の削除

--- sample.py
+++ sample.py
@@ -3,10 +3,9 @@ import MySBdt
 telnum = "電話番号"
 password = "MySoftbankのパスワード"
 line_access_token = "LineNotifyのアクセストークン"
-current_month_data = "契約しているプランのデータ量の少数第一まで 例: 5.0"
 
 # インスタンスの作成
-api = MySBdt.API(telnum=telnum, password=password, line_access_token=line_access_token, current_month_data=current_month_data)
+api = MySBdt.API(telnum=telnum, password=password, line_access_token=line_access_token)
 
 # データ（使用量、残量、繰越残量、追加量、追加使用量、追加繰越量、追加繰越使用量）の取得
 api.get_data()
