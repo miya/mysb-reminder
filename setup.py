dca1e82e80d863a9afd0759bf5c43dc6b5d12139
m0zu <m0zurillex@gmail.com>
2020-04-23 14:48:24 +0900

    Version 5.0 release 🚀

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-import os
 from setuptools import setup, find_packages
 
-install_reqs = [line.strip() for line in open('requirements.txt').readlines()]
+install_requires = [line.strip() for line in open("requirements.txt").readlines()]
 
 setup(
-    name='MySB_datatraffic',
-    version='4.1',
-    description='MySoftBank dataTraffic',
-    url='https://github.com/0x0u/MySB_dataTraffic',
-    author='m0zu',
-    author_email='m0zurillex@gmail.com',
-    keywords='Python3 MySoftBank',
+    name="mysb-scraping",
+    version="5.0",
+    description="Remaining data notifications with LINE Notify",
+    url="https://github.com/miya/mysb-scraping",
+    author="miya",
+    author_email="m0zurillex@gmail.com",
+    keywords=["softbank", "scraping"],
     packages=find_packages(),
-    install_requires=install_reqs,
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
