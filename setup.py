dae3224b0644ecb61d597a7c189c31ab7e1bd656
m0zu <m0zurillex@gmail.com>
2020-04-23 17:26:46 +0900

    Version 5.1 release 🚀

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 install_requires = [line.strip() for line in open("requirements.txt").readlines()]
 
 setup(
-    name="mysb-scraping",
-    version="5.0",
+    name="mysb-reminder",
+    version="5.1",
     description="Remaining data notifications with LINE Notify",
-    url="https://github.com/miya/mysb-scraping",
+    url="https://github.com/miya/mysb-reminder",
     author="miya",
     author_email="m0zurillex@gmail.com",
-    keywords=["softbank", "scraping"],
+    keywords=["softbank", "scraping", "LINE Notify"],
     packages=find_packages(),
     install_requires=install_requires,
     long_description=open("README.md").read(),
